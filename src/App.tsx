5e565e9c8fa6be0675e3f2fcd3452c8722645f42
Rafael Naves <r.naves21@gmail.com>
2023-01-17 16:40:00 -0300

    Adjusts tailwind and main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import { Habit } from './components/Habit'
 function App() {
 
   return (
-    <Habit completed={3}/>
+    <div>
+      <Habit completed={3}/>
+      <Habit completed={5}/>
+      <Habit completed={7}/>
+      <Habit completed={9}/>
+    </div>
   )
 }
 
